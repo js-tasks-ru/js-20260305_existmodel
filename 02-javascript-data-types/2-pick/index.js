07aad1d31baef473af9bb40d09e59798e7e07b2d
existmodel <sarizava@yandex.ru>
2026-03-11 13:32:03 +0100

    add solution for 02-javascript-data-types/2-pick

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -4,6 +4,20 @@
  * @param {...string} fields - the properties paths to pick
  * @returns {object} - returns the new object
  */
-export const pick = (obj, ...fields) => {
 
+//объект и произвольное количество строк – ключей объекта
+export const pick = (obj, ...fields) => {
+  let arr = Object.entries(obj);
+  let filteredArr = arr.filter(([key]) => fields.includes(key));
+  let result = Object.fromEntries(filteredArr);
+  return result;
 };
+// const fruits = {
+//   apple: 2,
+//   orange: 4,
+//   banana: 3,
+// };
+
+// pick(fruits, "apple", "banana");
+
+// console.log(pick(fruits, "apple", "banana"));
