5a6efbd92dd58fcfcc02b981b7887b1f9b25c5c2
existmodel <sarizava@yandex.ru>
2026-03-11 13:39:54 +0100

    add solution for 02-javascript-data-types/3-omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,18 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  let arr = Object.entries(obj);
+  let filteredArr = arr.filter(([key]) => !fields.includes(key));
+  let result = Object.fromEntries(filteredArr);
+  return result;
 };
+
+const fruits = {
+  apple: 2,
+  orange: 4,
+  banana: 3,
+};
+
+omit(fruits, "apple", "banana");
+
+console.log(omit(fruits, "apple", "banana"));
