c4f9907531fd4b7a5f44bbe72e3aadbf3ccf3e51
existmodel <sarizava@yandex.ru>
2026-03-16 17:35:08 +0100

    add solution for 03-objects-arrays-intro-to-testing/1-create-getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,44 +4,22 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-  // позволяет выбрать значение из объекта.
-  // return (obj) =>
-  // {return let result = obj.`${path}`}
-  // разделить строку на части
-  // Хороший вопрос! В вашем случае нужно разделить строку по точкам и пройтись по цепочке свойств объекта.
-  // Строка "props.images.src" должна стать путём: obj.props.images.src
-  // Вот решение:
-  // как обьединить строку и обьект?
-  //   возвращать новую функцию, которая по заданному пути найдет значение в переданном ей объекте и вернет это значение.
-  // мы получаем строку но нужно сделать так чтобы это был пусть к свойству
+  const keys = path.split(".");
+
+  return function (obj) {
+    let result = obj;
+
+    for (const key of keys) {
+      if (
+        result === undefined ||
+        result === null ||
+        !Object.hasOwn(result, key)
+      ) {
+        return undefined;
+      }
+      result = result[key];
+    }
+
+    return result;
+  };
 }
-
-// Необходимо реализовать функцию «createGetter». Функция должна принимать строку вида «prop-1.prop-2.prop-n», где «prop-1, …, prop-n» – это свойства объекта разделенные точкой.
-
-// К примеру строка вида «props.images.src» – это путь к свойству «src» следующего объекта:
-
-// const obj = {
-//   props: {
-//     images: {
-//       src: "http://path-to-some-img"
-//     }
-//   }
-// };
-// Функция «createGetter» должна возвращать новую функцию, которая по заданному пути найдет значение в переданном ей объекте и вернет это значение.
-
-// function createGetter(field) {
-//   /* ... */
-// }
-
-// const product = {
-//   category: {
-//     title: "Goods"
-//   }
-// }
-
-// const getter = createGetter('category.title');
-
-// console.log(getter(product)); // Goods
-
-//! разбить сплитом на части
-//!динамическое обращение к свойствам массива
